4c263f3ed59b01069b382e0e65462c2f942a3537
Joshua Elsasser <joshua@elsasser.org>
2022-02-28 20:35:58 -0800

    Add a workaround for an osx bug

--- setup.sh
+++ setup.sh
@@ -2,5 +2,11 @@
 
 set -e
 
+case "$(uname -sp)" in
+    Darwin\ i386)
+        export ARCHFLAGS='-arch x86_64'
+        ;;
+esac
+
 python3 -m venv venv
 ./venv/bin/pip install appdirs requests web3
